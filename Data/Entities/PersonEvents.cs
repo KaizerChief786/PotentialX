85aafe9c08c494802654af83c869736fe4d1b164
kaizerchief786 <Ash@satarahope.com>
2021-07-09 10:22:06 +1000

    adding person events

--- Data/Entities/PersonEvents.cs
+++ Data/Entities/PersonEvents.cs
@@ -5,14 +5,14 @@ using System.Threading.Tasks;
 
 namespace PotentialX.Data.Entities
 {
-    public class Person
+    public class PersonEvents
     {
         public int Id { get; set; }
         public string Forename { get; set; }
         public string Surname { get; set; }
         public string DOB { get; set; }
-        public string CreatedDate { get; set; }
-
-
+        public string EventType { get; set; }
+        public string EventDescription { get; set; }
+        public string EventDate { get; set; }
     }
 }
