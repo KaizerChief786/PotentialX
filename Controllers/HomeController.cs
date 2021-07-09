034f19a603ddd97e1444c8e8c8676a5f751e847d
kaizerchief786 <Ash@satarahope.com>
2021-07-09 10:32:26 +1000

    Fix compile issues

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using PotentialX.Data;
 using PotentialX.Models;
 using System;
 using System.Collections.Generic;
