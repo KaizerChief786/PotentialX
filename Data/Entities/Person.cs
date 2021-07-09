34eae13b4a945df6c376a91ed9f0e0bf34367d6d
kaizerchief786 <Ash@satarahope.com>
2021-07-09 10:19:27 +1000

    Create a Person entity

---
+++ Data/Entities/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PotentialX.Data.Entities
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public DateTime DOB { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+
+    }
+}
